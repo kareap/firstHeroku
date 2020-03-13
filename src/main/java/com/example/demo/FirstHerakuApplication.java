4430f4c7153951259f773c94e84503f6297565ee
KarolineE <eap-k@hotmail.com>
2020-03-13 11:15:11 +0100

    added postgresql

--- src/main/java/com/example/demo/FirstHerakuApplication.java
+++ src/main/java/com/example/demo/FirstHerakuApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,13 +8,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @SpringBootApplication
 @RestController
 public class FirstHerakuApplication {
 
+    @Autowired
+    DataSource dataSource;
+
     public static void main(String[] args) {
         SpringApplication.run(FirstHerakuApplication.class, args);
+    }
+
+    @GetMapping("/db")
+    public String dbVersion() throws SQLException {
+        Statement statement = dataSource.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery("select version();");
+        if (resultSet.next()) {
+            return resultSet.getString(1);
+        }
+        return null;
     }
 
     @GetMapping("/")
